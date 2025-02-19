4cb64cc7c4b5f73e12afb7989d978c80096b7dcd
NRC7 <n.rodriguez.c7@gmail.com>
2025-02-19 20:10:59 -0300

    Refactorizar componente SearchBar y corregir estilos en css

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { getCachedProducts } from '../util/CachedProducs';
 import { Link } from "react-router-dom";
-import '../styles/NavBar.css'
+import '../styles/SearchBar.css'
 
 const SearchBar = () => {
 
@@ -15,32 +15,38 @@ const SearchBar = () => {
         const data = await getCachedProducts();
         setProducts(data);
     };
+
+    function handleSearch(event) {
+        const userInput = event.target.value
+        if (userInput.length > 1) {
+
+            // Actualizo el valor de userSearchTerms
+            const searchTerms = userInput.split(' ')
+            setSearchTerms(searchTerms)
+
+            // Actualizo el valor de userSearchResults
+            fetchCachedProducts();
+            const searchResults = products?.filter(product => 
+                (product.name.toLowerCase().includes(searchTerms[0].toLowerCase())) ||
+                (product.category.toLowerCase().includes(searchTerms[0].toLowerCase())) ||
+                (product.name.toLowerCase().includes(searchTerms[0].toLowerCase()) &&
+                product.category.toLowerCase().includes(searchTerms[1]?.toLowerCase()))
+            ).sort((a, b) => b.rating - a.rating);
+            setSearchResults(searchResults)
+
+            // `/search/${userSearchTerms[0]}/${userSearchTerms[1]}?sort=rating`
+            // 
+        }
+    }
     
     return (
         <div className="search-bar">
-            <input type="text" placeholder="Buscar productos..." onChange={(event) => {
-                const userInput = event.target.value
-                if (userInput.length > 1) {
-
-                    // Actualizo el valor de userSearchTerms
-                    const searchTerms = userInput.split(' ')
-                    setSearchTerms(searchTerms)
-
-                    // Actualizo el valor de userSearchResults
-                    fetchCachedProducts();
-                    const searchResults = products?.filter(product => 
-                        (product.name.toLowerCase().includes(searchTerms[0].toLowerCase())) ||
-                        (product.category.toLowerCase().includes(searchTerms[0].toLowerCase())) ||
-                        (product.name.toLowerCase().includes(searchTerms[0].toLowerCase()) &&
-                        product.category.toLowerCase().includes(searchTerms[1]?.toLowerCase()))
-                    ).sort((a, b) => b.rating - a.rating);
-                    setSearchResults(searchResults)
-                    // `/search/${userSearchTerms[0]}/${userSearchTerms[1]}?sort=rating`
-                    // 
-                }
-            }} />
+            <input type="text" 
+                placeholder="Buscar productos..." 
+                onChange={(event) => handleSearch(event)} 
+            />
             <button className="search-button">
-                <Link className="highlight" 
+                <Link
                     to={{
                         pathname: `/search/${userSearchTerms}`
                     }}
